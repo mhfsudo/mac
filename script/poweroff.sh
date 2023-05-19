2a36eb8c98a9ed33bd831c2cf667afe9a10c1acb
Michael Friderich <michael@frideri.ch>
2023-05-19 21:36:43 +0200

    add poweroff.sh script

---
+++ script/poweroff.sh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+
+echo "=============================================================================="
+echo "                                      "
+echo "       __    ___  ___   ___  _______	"
+echo "      /  \__/  / /  /  /  / /  ____/  "
+echo "     /        / /  /__/  / /  /__     "
+echo "    /  /\_/  / /  ___   / /  ___/     "
+echo "   /  /  /  / /  /  /  / /  /		    "
+echo "  /__/  /__/ /__/  /__/ /__/ 		    "
+echo "                                      "
+echo "======================================"
+echo ""
+echo "Good, let's update your system"
+echo ""
+
+brew update
+brew upgrade
+softwareupdate -l
+sudo shutdown -h now
+
+echo ""
+echo "Good, we're done"
+echo ""
+echo "=============================================================================="
